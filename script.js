3121dbd9fe3137eb27c5d105a3c74be602f7b58d
Raj Rasane <rasaneraj04@gmail.com>
2024-02-27 22:53:28 +0530

    fuctions updated

--- script.js
+++ script.js
@@ -115,6 +115,7 @@ list.addEventListener("click", (event) => {
   }
 });
 
+// Keeps track of item added even after refreshing
 document.addEventListener("DOMContentLoaded", () => {
   const ref = localStorage.getItem("demoarray");
   if (ref) {
@@ -124,29 +125,3 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
-
-const toggleSwitch = document.querySelector(
-  '.theme-switch input[type="checkbox"]'
-);
-const currentTheme = localStorage.getItem("theme");
-
-if (currentTheme) {
-  document.documentElement.setAttribute("data-theme", currentTheme);
-
-  if (currentTheme === "dark") {
-    toggleSwitch.checked = true;
-  }
-}
-
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "light");
-  }
-}
-
-toggleSwitch.addEventListener("change", switchTheme, false);
-
